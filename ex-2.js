49a08122a6a82fd36c458287f8655f5cf2ea41e0
83mhpll <dogk678@gmail.com>
2025-07-18 14:39:42 +0700

    feat: implement light bulb status checks and grading function

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,18 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "Broken";
 
-// Start coding here.
+if (lightBulbStatus === "On") {
+    console.log("light buld is ON.");
+    
+}
+    else if (lightBulbStatus === "off") {
+    console.log("light buld is off");
+
+}
+
+   else if (lightBulbStatus === "Broken") {
+    console.log("light bild is Broken.");
+
+} else {
+    console.log("Please choose the correct input (On/Off/Broken)");
+}
--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,10 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+console.log(
+    lightBulbStatus === "On"
+    ? "light buld is on."
+    : lightBulbStatus === "Off"
+    ? "light buld is Off"
+    : "Please choose the correct input (On/Off)"
+);
